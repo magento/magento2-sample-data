c038a7968aaed207137ddcbe8972855a0e03a13d
Volodymyr Klymenko <vklymenko@ebay.com>
2015-02-20 11:39:53 +0200

    MAGETWO-33270: Modify publication scripts to publish Sample Data
     - Move sample data media to sample data repository

---
+++ dev/tests/integration/testsuite/Magento/Tools/SampleData/Helper/TestFixture.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Tools\SampleData\Helper;
+
+
+class TestFixture extends Fixture
+{
+    public function getPath($subPath)
+    {
+        $file = realpath(__DIR__ . '/../_files/fixtures/' . ltrim($subPath, '/'));
+        if (file_exists($file)) {
+            return $file;
+
+        }
+        return parent::getPath($subPath);
+    }
+}
