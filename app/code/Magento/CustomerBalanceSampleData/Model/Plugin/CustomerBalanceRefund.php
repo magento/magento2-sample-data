d11877adfc03f90787a8f1ff3e53b65fd720d92b
Leonid Poluyanov <lpoluyanov@ebay.com>
2015-09-09 16:55:18 +0300

    MAGETWO-41728: Move SD into modules (p4)

--- app/code/Magento/CustomerBalanceSampleData/Model/Plugin/CustomerBalanceRefund.php
+++ app/code/Magento/CustomerBalanceSampleData/Model/Plugin/CustomerBalanceRefund.php
@@ -3,22 +3,23 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\CustomerBalanceSampleData\Model;
+namespace Magento\CustomerBalanceSampleData\Model\Plugin;
 
 /**
  * Class Observer
  */
-class Observer
+class CustomerBalanceRefund
 {
     /**
-     * @param \Magento\Framework\DataObject $params
+     * @param \Magento\Sales\Model\Order\Item $orderItem
      * @return mixed
      */
-    public function getCreditmemoData(\Magento\Framework\DataObject $params)
-    {
-        /** @var \Magento\Sales\Model\Order\Item $orderItem */
-        $orderItem = $params->getOrderItem();
-        $data = $params->getCreditMemo();
+    public function aroundGetCreditmemoData(
+        \Magento\SalesSampleData\Model\Order\Processor $subject,
+        \Closure $proceed,
+        \Magento\Sales\Model\Order\Item $orderItem
+    ) {
+        $data = $proceed($orderItem);
         if ($orderItem->getOrder()->getBaseGrandTotal()) {
             $data['refund_customerbalance_return_enable'] = '1';
             $data['refund_customerbalance_return'] = $orderItem->getOrder()->getBaseGrandTotal();
