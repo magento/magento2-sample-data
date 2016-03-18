ddfc960eaa4374b19e760409ae58047bce0fe14e
Leonid Poluyanov <lpoluyanov@ebay.com>
2016-03-18 19:49:03 +0200

    MAGETWO-50415: Grouped product has no products in sample data

--- app/code/Magento/GroupedProductSampleData/Model/Product.php
+++ app/code/Magento/GroupedProductSampleData/Model/Product.php
@@ -5,8 +5,8 @@
  */
 namespace Magento\GroupedProductSampleData\Model;
 
-use Magento\Framework\Setup\SampleData\Context as SampleDataContext;
 use Magento\Catalog\Model\Product\Initialization\Helper\ProductLinks;
+use \Magento\Framework\App\ObjectManager;
 
 /**
  * Setup grouped product
@@ -21,42 +21,7 @@ class Product extends \Magento\CatalogSampleData\Model\Product
     /**
      * @var \Magento\Catalog\Model\Product\Initialization\Helper\ProductLinks
      */
-    protected $productLinksHelper;
-
-    /**
-     * Product constructor.
-     * @param SampleDataContext $sampleDataContext
-     * @param \Magento\Catalog\Model\ProductFactory $productFactory
-     * @param \Magento\Catalog\Model\ConfigFactory $catalogConfig
-     * @param \Magento\GroupedProductSampleData\Model\Product\Converter $converter
-     * @param \Magento\Framework\Setup\SampleData\FixtureManager $fixtureManager
-     * @param \Magento\CatalogSampleData\Model\Product\Gallery $gallery
-     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
-     * @param \Magento\Eav\Model\Config $eavConfig
-     * @param \Magento\Catalog\Model\Product\Initialization\Helper\ProductLinks $productLinksHelper
-     */
-    public function __construct(
-        SampleDataContext $sampleDataContext,
-        \Magento\Catalog\Model\ProductFactory $productFactory,
-        \Magento\Catalog\Model\ConfigFactory $catalogConfig,
-        \Magento\GroupedProductSampleData\Model\Product\Converter $converter,
-        \Magento\Framework\Setup\SampleData\FixtureManager $fixtureManager,
-        \Magento\CatalogSampleData\Model\Product\Gallery $gallery,
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Eav\Model\Config $eavConfig,
-        \Magento\Catalog\Model\Product\Initialization\Helper\ProductLinks $productLinksHelper
-    ) {
-        parent::__construct(
-            $sampleDataContext,
-            $productFactory,
-            $catalogConfig,
-            $converter,
-            $gallery,
-            $storeManager,
-            $eavConfig
-        );
-        $this->productLinksHelper = $productLinksHelper;
-    }
+    private $productLinksHelper;
 
     /**
      * @param \Magento\Catalog\Model\Product $product
@@ -65,8 +30,26 @@ class Product extends \Magento\CatalogSampleData\Model\Product
      */
     protected function prepareProduct($product, $data)
     {
-        $this->productLinksHelper->initializeLinks($product, $data['grouped_link_data']);
+        $this->getProductLinksHelper()->initializeLinks($product, $data['grouped_link_data']);
         $product->unsetData('grouped_link_data');
         return $this;
+    }
+
+    /**
+     * Get product links helper
+     *
+     * @deprecated
+     * @return \Magento\Catalog\Model\Product\Initialization\Helper\ProductLinks
+     */
+    private function getProductLinksHelper()
+    {
+
+        if (!($this->productLinksHelper)) {
+            return ObjectManager::getInstance()->get(
+                '\Magento\Catalog\Model\Product\Initialization\Helper\ProductLinks'
+            );
+        } else {
+            return $this->productLinksHelper;
+        }
     }
 }
