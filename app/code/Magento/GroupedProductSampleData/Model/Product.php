4db43528fb4d8848c2f8d871ff496a11a9c86992
Yaroslav Onischenko <yonischenko@magento.com>
2016-03-23 13:34:53 +0200

    MAGETWO-50415: Grouped product has no products in sample data

--- app/code/Magento/GroupedProductSampleData/Model/Product.php
+++ app/code/Magento/GroupedProductSampleData/Model/Product.php
@@ -5,6 +5,7 @@
  */
 namespace Magento\GroupedProductSampleData\Model;
 
+use Magento\Framework\Setup\SampleData\Context as SampleDataContext;
 use Magento\Catalog\Model\Product\Initialization\Helper\ProductLinks;
 use \Magento\Framework\App\ObjectManager;
 
@@ -22,6 +23,38 @@ class Product extends \Magento\CatalogSampleData\Model\Product
      * @var \Magento\Catalog\Model\Product\Initialization\Helper\ProductLinks
      */
     private $productLinksHelper;
+
+    /**
+     * Product constructor.
+     * @param SampleDataContext $sampleDataContext
+     * @param \Magento\Catalog\Model\ProductFactory $productFactory
+     * @param \Magento\Catalog\Model\ConfigFactory $catalogConfig
+     * @param \Magento\GroupedProductSampleData\Model\Product\Converter $converter
+     * @param \Magento\Framework\Setup\SampleData\FixtureManager $fixtureManager
+     * @param \Magento\CatalogSampleData\Model\Product\Gallery $gallery
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Eav\Model\Config $eavConfig
+     */
+    public function __construct(
+        SampleDataContext $sampleDataContext,
+        \Magento\Catalog\Model\ProductFactory $productFactory,
+        \Magento\Catalog\Model\ConfigFactory $catalogConfig,
+        \Magento\GroupedProductSampleData\Model\Product\Converter $converter,
+        \Magento\Framework\Setup\SampleData\FixtureManager $fixtureManager,
+        \Magento\CatalogSampleData\Model\Product\Gallery $gallery,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Eav\Model\Config $eavConfig
+    ) {
+        parent::__construct(
+            $sampleDataContext,
+            $productFactory,
+            $catalogConfig,
+            $converter,
+            $gallery,
+            $storeManager,
+            $eavConfig
+        );
+    }
 
     /**
      * @param \Magento\Catalog\Model\Product $product
