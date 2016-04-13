da565df0287b293f0c9b1d67ccee9414afba86be
Leonid Poluyanov <lpoluyanov@ebay.com>
2016-04-13 12:44:21 +0300

    MAGETWO-50649: Downloadable product has no links and samples in sample data

--- app/code/Magento/DownloadableSampleData/Model/Product.php
+++ app/code/Magento/DownloadableSampleData/Model/Product.php
@@ -6,6 +6,9 @@
 namespace Magento\DownloadableSampleData\Model;
 
 use Magento\Framework\Setup\SampleData\Context as SampleDataContext;
+use Magento\Downloadable\Api\Data\SampleInterfaceFactory as SampleFactory;
+use Magento\Downloadable\Api\Data\LinkInterfaceFactory as LinkFactory;
+use \Magento\Framework\App\ObjectManager;
 
 /**
  * Setup downloadable product
@@ -28,6 +31,16 @@ class Product extends \Magento\CatalogSampleData\Model\Product
     protected $downloadableData = [];
 
     /**
+     * @var SampleFactory
+     */
+    protected $sampleFactory;
+
+    /**
+     * @var LinkFactory
+     */
+    protected $linkFactory;
+
+    /**
      * Product constructor.
      * @param SampleDataContext $sampleDataContext
      * @param \Magento\Catalog\Model\ProductFactory $productFactory
@@ -44,7 +57,9 @@ class Product extends \Magento\CatalogSampleData\Model\Product
         \Magento\DownloadableSampleData\Model\Product\Converter $converter,
         \Magento\CatalogSampleData\Model\Product\Gallery $gallery,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Eav\Model\Config $eavConfig
+        \Magento\Eav\Model\Config $eavConfig,
+        SampleFactory $sampleFactory,
+        LinkFactory $linkFactory
     ) {
         parent::__construct(
             $sampleDataContext,
@@ -55,6 +70,8 @@ class Product extends \Magento\CatalogSampleData\Model\Product
             $storeManager,
             $eavConfig
         );
+        $this->linkFactory = $linkFactory;
+        $this->sampleFactory = $sampleFactory;
     }
 
     /**
@@ -96,9 +113,109 @@ class Product extends \Magento\CatalogSampleData\Model\Product
     protected function prepareProduct($product, $data)
     {
         if (isset($this->downloadableData[$data['sku']])) {
+            $extension = $product->getExtensionAttributes();
+            $links = [];
+            foreach ($this->downloadableData[$data['sku']]['link'] as $linkData) {
+                $link = $this->getLinkFactory()->create(['data' => $linkData]);
+                if (isset($linkData['type'])) {
+                    $link->setLinkType($linkData['type']);
+                }
+                if (isset($linkData['file'])) {
+                    $link->setFile($linkData['file']);
+                }
+                if (isset($linkData['file_content'])) {
+                    $link->setLinkFileContent($linkData['file_content']);
+                }
+                $link->setId(null);
+                if (isset($linkData['sample']['type'])) {
+                    $link->setSampleType($linkData['sample']['type']);
+                }
+                if (isset($linkData['sample']['file'])) {
+                    $link->setSampleFileData($linkData['sample']['file']);
+                }
+                if (isset($linkData['sample']['url'])) {
+                    $link->setSampleUrl($linkData['sample']['url']);
+                }
+                if (isset($linkData['sample']['file_content'])) {
+                    $link->setSampleFileContent($linkData['file_content']);
+                }
+                $link->setStoreId($product->getStoreId());
+                $link->setWebsiteId($product->getStore()->getWebsiteId());
+                $link->setProductWebsiteIds($product->getWebsiteIds());
+                if (!$link->getSortOrder()) {
+                    $link->setSortOrder(1);
+                }
+                if (null === $link->getPrice()) {
+                    $link->setPrice(0);
+                }
+                if ($link->getIsUnlimited()) {
+                    $link->setNumberOfDownloads(0);
+                }
+                $links[] = $link;
+            }
+            $extension->setDownloadableProductLinks($links);
+
+            $samples = [];
+            foreach ($this->downloadableData[$data['sku']]['sample'] as $sampleData) {
+                $sample = $this->getSampleFactory()->create(['data' => $sampleData]);
+                $sample->setId(null);
+                $sample->setStoreId($product->getStoreId());
+                if (isset($sampleData['type'])) {
+                    $sample->setSampleType($sampleData['type']);
+                }
+                if (isset($sampleData['file'])) {
+                    $sample->setFile($sampleData['file']);
+                }
+                if (isset($sampleData['sample_url'])) {
+                    $sample->setSampleUrl($sampleData['sample_url']);
+                }
+                if (!$sample->getSortOrder()) {
+                    $sample->setSortOrder(1);
+                }
+                $samples[] = $sample;
+            }
+            $extension->setDownloadableProductSamples($samples);
+
             $product->setDownloadableData($this->downloadableData[$data['sku']]);
+            $product->setExtensionAttributes($extension);
         }
         $this->setVirtualStockData($product);
         return $this;
+    }
+
+    /**
+     * Get link interface factory
+     *
+     * @deprecated
+     * @return \Magento\Downloadable\Api\Data\LinkInterfaceFactory
+     */
+    private function getLinkFactory()
+    {
+
+        if (!($this->linkFactory)) {
+            return ObjectManager::getInstance()->get(
+                '\Magento\Downloadable\Api\Data\LinkInterfaceFactory'
+            );
+        } else {
+            return $this->linkFactory;
+        }
+    }
+
+    /**
+     * Get sample interface factory
+     *
+     * @deprecated
+     * @return \Magento\Downloadable\Api\Data\SampleInterfaceFactory
+     */
+    private function getSampleFactory()
+    {
+
+        if (!($this->sampleFactory)) {
+            return ObjectManager::getInstance()->get(
+                '\Magento\Downloadable\Api\Data\SampleInterfaceFactory'
+            );
+        } else {
+            return $this->sampleFactory;
+        }
     }
 }
