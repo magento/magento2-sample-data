2f77ee777dd746df6386f978881ab3ddd636326c
Andrii Meysar <andrii.meysar@transoftgroup.com>
2017-06-16 18:43:11 +0300

    MAGETWO-64499: Error during Sample Data deploying using " auto_increment_increment = 3" as Mysql option

--- app/code/Magento/TaxSampleData/Model/Tax.php
+++ app/code/Magento/TaxSampleData/Model/Tax.php
@@ -5,10 +5,13 @@
  */
 namespace Magento\TaxSampleData\Model;
 
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Setup\SampleData\Context as SampleDataContext;
 
 /**
  * Class Tax
+ *
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
 class Tax
 {
@@ -58,6 +61,13 @@ class Tax
     protected $csvReader;
 
     /**
+     * Region collection factory.
+     *
+     * @var \Magento\Directory\Model\ResourceModel\Region\CollectionFactory
+     */
+    private $regionCollectionFactory;
+
+    /**
      * @param SampleDataContext $sampleDataContext
      * @param \Magento\Tax\Api\TaxRuleRepositoryInterface $taxRuleRepository
      * @param \Magento\Tax\Api\Data\TaxRuleInterfaceFactory $ruleFactory
@@ -66,6 +76,7 @@ class Tax
      * @param \Magento\Tax\Model\Calculation\RateFactory $taxRateFactory
      * @param \Magento\Framework\Api\SearchCriteriaBuilder $criteriaBuilder
      * @param \Magento\Framework\Api\FilterBuilder $filterBuilder
+     * @param \Magento\Directory\Model\ResourceModel\Region\CollectionFactory $regionCollectionFactory
      */
     public function __construct(
         SampleDataContext $sampleDataContext,
@@ -75,7 +86,8 @@ class Tax
         \Magento\Tax\Api\Data\TaxRateInterfaceFactory $rateFactory,
         \Magento\Tax\Model\Calculation\RateFactory $taxRateFactory,
         \Magento\Framework\Api\SearchCriteriaBuilder $criteriaBuilder,
-        \Magento\Framework\Api\FilterBuilder $filterBuilder
+        \Magento\Framework\Api\FilterBuilder $filterBuilder,
+        \Magento\Directory\Model\ResourceModel\Region\CollectionFactory $regionCollectionFactory = null
     ) {
         $this->fixtureManager = $sampleDataContext->getFixtureManager();
         $this->csvReader = $sampleDataContext->getCsvReader();
@@ -86,12 +98,28 @@ class Tax
         $this->taxRateFactory = $taxRateFactory;
         $this->criteriaBuilder = $criteriaBuilder;
         $this->filterBuilder = $filterBuilder;
+        $this->regionCollectionFactory = $regionCollectionFactory ?: ObjectManager::getInstance()->get(
+            \Magento\Directory\Model\ResourceModel\Region\CollectionFactory::class
+        );
     }
 
     /**
      * {@inheritdoc}
      */
     public function install(array $fixtures)
+    {
+        $this->createTaxRates($fixtures);
+        $this->createTaxRules();
+    }
+
+    /**
+     * Create tax rates.
+     *
+     * @param array $fixtures
+     * @return void
+     * @throws \Exception
+     */
+    private function createTaxRates(array $fixtures)
     {
         foreach ($fixtures as $fileName) {
             $fileName = $this->fixtureManager->getFixture($fileName);
@@ -111,50 +139,76 @@ class Tax
                     continue;
                 }
                 $taxRate = $this->rateFactory->create();
+                $regionId = $this->getRegionId($data['tax_region_name'], $data['tax_country_id']);
                 $taxRate->setCode($data['code'])
                     ->setTaxCountryId($data['tax_country_id'])
-                    ->setTaxRegionId($data['tax_region_id'])
+                    ->setTaxRegionId($regionId)
                     ->setTaxPostcode($data['tax_postcode'])
                     ->setRate($data['rate']);
                 $this->taxRateRepository->save($taxRate);
             }
+        }
+    }
 
-            $fixtureFile = 'Magento_TaxSampleData::fixtures/tax_rule.csv';
-            $fixtureFileName = $this->fixtureManager->getFixture($fixtureFile);
-            if (!file_exists($fileName)) {
+    /**
+     * Create tax rules.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    private function createTaxRules()
+    {
+        $fixtureFile = 'Magento_TaxSampleData::fixtures/tax_rule.csv';
+        $fixtureFileName = $this->fixtureManager->getFixture($fixtureFile);
+        if (!file_exists($fixtureFileName)) {
+            return;
+        }
+
+        $rows = $this->csvReader->getData($fixtureFileName);
+        $header = array_shift($rows);
+
+        foreach ($rows as $row) {
+            $data = [];
+            foreach ($row as $key => $value) {
+                $data[$header[$key]] = $value;
+            }
+            $filter = $this->filterBuilder->setField('code')
+                ->setConditionType('=')
+                ->setValue($data['code'])
+                ->create();
+            $criteria = $this->criteriaBuilder->addFilters([$filter])->create();
+            $existingRates = $this->taxRuleRepository->getList($criteria)->getItems();
+            if (!empty($existingRates)) {
                 continue;
             }
 
-            $rows = $this->csvReader->getData($fixtureFileName);
-            $header = array_shift($rows);
-
-            foreach ($rows as $row) {
-                $data = [];
-                foreach ($row as $key => $value) {
-                    $data[$header[$key]] = $value;
-                }
-                $filter = $this->filterBuilder->setField('code')
-                    ->setConditionType('=')
-                    ->setValue($data['code'])
-                    ->create();
-                $criteria = $this->criteriaBuilder->addFilters([$filter])->create();
-                $existingRates = $this->taxRuleRepository->getList($criteria)->getItems();
-                if (!empty($existingRates)) {
-                    continue;
-                }
-
-                $taxRate = $this->taxRateFactory->create()->loadByCode($data['tax_rate']);
-                $taxRule = $this->ruleFactory->create();
-                $taxRule->setCode($data['code'])
-                    ->setTaxRateIds([$taxRate->getId()])
-                    ->setCustomerTaxClassIds([$data['tax_customer_class']])
-                    ->setProductTaxClassIds([$data['tax_product_class']])
-                    ->setPriority($data['priority'])
-                    ->setCalculateSubtotal($data['calculate_subtotal'])
-                    ->setPosition($data['position']);
-                $this->taxRuleRepository->save($taxRule);
-            }
+            $taxRate = $this->taxRateFactory->create()->loadByCode($data['tax_rate']);
+            $taxRule = $this->ruleFactory->create();
+            $taxRule->setCode($data['code'])
+                ->setTaxRateIds([$taxRate->getId()])
+                ->setCustomerTaxClassIds([$data['tax_customer_class']])
+                ->setProductTaxClassIds([$data['tax_product_class']])
+                ->setPriority($data['priority'])
+                ->setCalculateSubtotal($data['calculate_subtotal'])
+                ->setPosition($data['position']);
+            $this->taxRuleRepository->save($taxRule);
         }
+    }
 
+    /**
+     * Return region Id by code or name.
+     *
+     * @param string $region
+     * @param string $countryId
+     * @return mixed
+     */
+    private function getRegionId($region, $countryId)
+    {
+        $regionCollection = $this->regionCollectionFactory->create();
+        $regionCollection->addCountryFilter($countryId)
+            ->addRegionCodeOrNameFilter($region)
+            ->setPageSize(1);
+
+        return $regionCollection->getFirstItem()->getId();
     }
 }
