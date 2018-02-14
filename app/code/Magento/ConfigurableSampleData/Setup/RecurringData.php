bf29514e1102de804be7c96e7d010cfa049de3cb
Roman Ganin <rganin@magento.com>
2018-02-14 11:39:27 +0200

    MAGETWO-87551: Convert existing data install/upgrade scripts
    - reindex in configurable

--- app/code/Magento/ConfigurableSampleData/Setup/RecurringData.php
+++ app/code/Magento/ConfigurableSampleData/Setup/RecurringData.php
@@ -4,7 +4,7 @@
  * See COPYING.txt for license details.
  */
 
-namespace Magento\CatalogRuleSampleData\Setup;
+namespace Magento\ConfigurableSampleData\Setup;
 
 use Magento\Framework\App\State;
 use Magento\Framework\Indexer\IndexerInterfaceFactory;
